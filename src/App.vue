497404f301da85566ccba5c577e13cb545d18b8b
thanakritcankha <68786794+thanakritcankha@users.noreply.github.com>
2022-03-28 21:25:26 +0700

    asdf

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <style>
